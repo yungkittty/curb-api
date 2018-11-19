09937fd5d299ab395ffbbecc497576438c16973d
Quentin Collin <quentin.collin@epitech.eu>
2018-11-20 02:27:52 +0800

    added - in route (~sign-in), sign-out request only require token, dummies refacto

--- services/sign-in/src/controllers/sign-out.js
+++ services/sign-in/src/controllers/sign-out.js
@@ -1,14 +1,10 @@
 const authService = require('../services/authentication');
 
 async function signOut(req, res) {
-  const { login, password, refreshToken } = req.body;
-  if (!login || !password || !refreshToken) return res.status(400).end();
+  const { token } = req.body;
+  if (!token) return res.status(400).end();
   try {
-    const response = await authService.logout({
-      login,
-      password,
-      refreshToken
-    });
+    const response = await authService.logout(token);
     if (!response) return res.status(400).end();
     return res.status(200).end();
   } catch (error) {
