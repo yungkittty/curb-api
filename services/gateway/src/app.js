8a4c1f06d07ac7f26952c43b8a1435439bafa73a
Elias Demnati <elias@ordoclic.fr>
2018-12-16 00:02:56 +0800

    Gateway + db in docker + seeds

--- services/gateway/src/app.js
+++ services/gateway/src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const proxy = require('express-http-proxy');
 
 const app = express();
 
@@ -13,5 +14,8 @@ if (process.env.NODE_ENV !== 'test') {
 app.get('/', (req, res) => {
   res.send(`${process.env.SERVICE_NAME} endpoint`);
 });
+
+app.use('/sign-in', proxy(process.env.CURB_SIGN_IN));
+app.use('/sign-up', proxy(process.env.CURB_SIGN_UP));
 
 module.exports = app;
--- services/users/src/services/create.js
+++ services/users/src/services/create.js
@@ -1,4 +1,4 @@
-const User = require('../../../../models/user');
+const User = require('../models/user');
 
 function create(newUser) {
   const currentDate = new Date();
