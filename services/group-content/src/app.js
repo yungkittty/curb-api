a279a1e788112c2d6a5a9e11afa4ba7a54684b12
Elias Demnati <elias.demnati@epitech.eu>
2018-12-19 16:11:09 +0800

    Create the microservice

---
+++ services/group-content/src/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+if (process.env.NODE_ENV !== 'test') {
+  app.use(morgan('dev'));
+}
+
+app.get('/', (req, res) => {
+  res.send(`${process.env.SERVICE_NAME} endpoint`);
+});
+
+module.exports = app;
