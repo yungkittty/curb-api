a1363fbdf146e5a1a52057428a5b6556403412a8
Quentin Collin <quentin.collin@epitech.eu>
2019-01-20 11:11:56 +0800

    avatarUrl for users/groups

--- services/groups/src/controllers/group-avatar.js
+++ services/groups/src/controllers/group-avatar.js
@@ -3,8 +3,8 @@ const avatar = require('../services/avatar');
 async function groupAvatar(req, res) {
   if (!req.body.avatarUrl || !req.params.groupId) return res.status(400).end();
   try {
-    const response = avatar(req.params.groupId, req.body.avatarUrl);
-    if (!response) return null;
+    const response = await avatar(req.params.groupId, req.body.avatarUrl);
+    if (!response) return res.status(200).end();
     return res.status(200).end();
   } catch (error) {
     return res.status(500).end();
--- services/users/src/controllers/index.js
+++ services/users/src/controllers/index.js
@@ -2,10 +2,12 @@ const userCreate = require('./user-create');
 const userRead = require('./user-read');
 const userUpdate = require('./user-update');
 const userDelete = require('./user-delete');
+const userAvatar = require('./user-avatar');
 
 module.exports = {
   userCreate,
   userRead,
   userUpdate,
-  userDelete
+  userDelete,
+  userAvatar
 };
