413b127de5cea0dd06de7374b469a82a4de9089f
Elias Demnati <elias.demnati@epitech.eu>
2019-01-23 11:44:07 +0100

    Remove serve middleware

--- services/group-content/src/serve.js
+++ services/group-content/src/serve.js
@@ -2,11 +2,6 @@ const express = require('express');
 
 const serve = express();
 
-serve.use((req, res, next) => {
-  console.log('Check right to get the requested static file here'); // eslint-disable-line
-  next();
-});
-
 serve.use(express.static('uploads'));
 serve.use(express.static('default'));
 
