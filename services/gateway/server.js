073eb2f9d11631a05a2dbcb85254cee37de560f2
Elias Demnati <elias.demnati@epitech.eu>
2019-03-07 15:40:36 +0800

    Add ssl to gateway (#50)

--- services/gateway/server.js
+++ services/gateway/server.js
@@ -1,4 +1,11 @@
 require('dotenv').config();
+const https = require('https');
+const fs = require('fs');
 const app = require('./src/app');
 
-app.listen(4000);
+https.createServer({
+  key: fs.readFileSync('./encryption/key.pem'),
+  cert: fs.readFileSync('./encryption/cert.pem'),
+  passphrase: '8xK58u33WwxCBiW',
+}, app)
+.listen(4000);
