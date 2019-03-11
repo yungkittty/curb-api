31d88fb600acabcffffc68069b3c3d3eaa90f884
Quentin Collin <quentin.collin@epitech.eu>
2019-03-11 11:32:44 +0800

    tag error

--- services/groups/src/services/delete-post.js
+++ services/groups/src/services/delete-post.js
@@ -3,7 +3,7 @@ const { ApiError } = require('../configurations/error');
 
 async function deletePost(groupId, mediaId) {
   const group = await Group.findById(groupId);
-  if (!group) throw new ApiError('Inexistent ressource');
+  if (!group) throw new ApiError('GROUP_NOT_FOUND');
   group.medias = group.medias.filter(media => media !== mediaId);
   await group.save();
   return group;
