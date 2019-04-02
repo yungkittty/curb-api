16268d47327f03ce8e6c16ac51fb1f15067f4036
Quentin Collin <quentin.collin@epitech.eu>
2019-04-02 18:58:18 +0800

    error

--- services/contents/src/app.js
+++ services/contents/src/app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const medias = require('./medias');
 const serve = require('./serve');
 const controllers = require('./controllers');
+const middlewares = require('./middleswares');
 
 const app = express();
 
@@ -23,7 +24,6 @@ app.get('/:contentId', controllers.contentRead);
 app.patch('/:contentId', controllers.contentUpdate);
 app.delete('/:contentId', controllers.contentDelete);
 
-
 app.use('/uploads', serve);
 app.use('/default', serve);
 app.use('/images', medias.images);
@@ -31,5 +31,7 @@ app.use('/videos', medias.videos);
 app.use('/locations', medias.locations);
 app.use('/avatars', medias.avatars);
 app.use('/texts', medias.texts);
+
+app.use(middlewares.error);
 
 module.exports = app;
--- services/contents/src/models/content.js
+++ services/contents/src/models/content.js
@@ -1,12 +1,20 @@
 const mongoose = require('mongoose');
+const { ApiError } = require('../configurations/error');
 
 mongoose.connect('mongodb://db/Curb', { useNewUrlParser: true });
 
 const contentSchema = mongoose.Schema({
-  creatorId: { type: String, required: true },
-  groupId: { type: String, required: true },
+  creatorId: { type: String, required: [true, 'MISSING_CREATOR_ID'] },
+  groupId: { type: String, required: [true, 'MISSING_GROUP_ID'] },
   dateCreation: Date,
-  type: { type: [String], required: true, enum: ['localisation', 'text', 'image', 'video'] },
+  type: {
+    type: String,
+    required: [true, 'MISSING_TYPE'],
+    enum: {
+      values: ['localisation', 'text', 'image', 'video'],
+      message: 'BAD_TYPE'
+    }
+  },
   data: String
 });
 
@@ -16,5 +24,20 @@ contentSchema.methods.getPublicFields = function() {
   const { __v, _id, ...content } = this.toObject();
   return { id: _id, ...content };
 };
+
+contentSchema.post('save', async (error, doc, next) => {
+  console.log('MONGO ERROR:', error);
+  if (error.name === 'MongoError' && error.code === 11000) {
+    return next(new ApiError('CONTENT_ALREADY_EXIST'));
+  }
+  if (error.errors[Object.keys(error.errors)[0]]) {
+    return next(
+      new ApiError(
+        error.errors[Object.keys(error.errors)[0]].message.toUpperCase()
+      )
+    );
+  }
+  return next(error);
+});
 
 module.exports = mongoose.model('contents', contentSchema);
