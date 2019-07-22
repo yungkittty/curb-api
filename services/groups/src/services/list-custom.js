d59400efa070dcf580f4695eedeae2de7cfb2142
Quentin Collin <quentin.collin@epitech.eu>
2019-07-22 12:25:00 +0200

    new user for custom

--- services/groups/src/services/list-custom.js
+++ services/groups/src/services/list-custom.js
@@ -5,6 +5,7 @@ const listRandom = require('./list-random');
 async function listCustom({
   page = 1, count = 5, category = undefined, userId
 }) {
+  const response = { page, count, section: 'custom' };
   const skip = (page - 1) * count;
 
   const groupCustomIds = await UserRecommendation.aggregate([
@@ -18,27 +19,36 @@ async function listCustom({
 
   const { ids = [] } = groupCustomIds[0] || [];
 
-  if (ids.length === 0 && (await UserRecommendation.findOne({ _id: userId })) === null) {
-    const { groups: groupIds } = await listRandom({ page: 1, count: 50 });
-    const newRecommendation = new UserRecommendation({
-      _id: mongoose.Types.ObjectId(userId),
-      groupIds
-    });
-    await newRecommendation.save();
-    const response = await listCustom({
-      page,
-      count,
-      category,
-      userId
-    });
-    return response;
+  if (ids.length === 0) {
+    const recommendation = await UserRecommendation.findOne({ _id: userId });
+    if (recommendation === null) {
+      const { groups: groupIds } = await listRandom({ page: 1, count: 50 });
+      const newRecommendation = new UserRecommendation({
+        _id: mongoose.Types.ObjectId(userId),
+        groupIds
+      });
+      await newRecommendation.save();
+      return await listCustom({
+        page,
+        count,
+        category,
+        userId
+      });
+    }
+    if (recommendation.groupIds.length === 0) {
+      const { groups: groupIds } = await listRandom({ page: 1, count: 50 });
+      recommendation.groupIds = groupIds;
+      await recommendation.save();
+      return await listCustom({
+        page,
+        count,
+        category,
+        userId
+      });
+    }
   }
-  return {
-    count,
-    page,
-    section: 'custom',
-    groups: ids.reduce((acc, id) => acc.concat(id), [])
-  };
+
+  return { ...response, groups: ids.reduce((acc, id) => acc.concat(id), []) };
 }
 
 module.exports = listCustom;
