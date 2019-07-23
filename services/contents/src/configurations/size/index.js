c8f17a8f7e2dfbb52b3c1c22ee1408da1f45a3bf
Antoine <antoine.ladinh@epitech.eu>
2019-07-23 21:34:11 +0800

    changement des tailles avatars

--- services/contents/src/configurations/size/index.js
+++ services/contents/src/configurations/size/index.js
@@ -14,6 +14,14 @@ const sizes = [
   {
     name: 'large',
     size: parseInt(process.env.AVATAR_SIZE_LARGE, 10)
+  },
+  {
+    name: 'extra_large',
+    size: parseInt(process.env.AVATAR_SIZE_EXTRA_LARGE, 10)
+  },
+  {
+    name: 'extra_extra_large',
+    size: parseInt(process.env.AVATAR_SIZE_EXTRA_EXTRA_LARGE, 10)
   }
 ];
 
