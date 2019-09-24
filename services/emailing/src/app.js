9e64532bc8b58c0f151a6edcf69060c0e91116e3
collin_q <quentin.collin@epitech.eu>
2019-09-24 21:32:32 +0200

    cors

--- services/emailing/src/app.js
+++ services/emailing/src/app.js
@@ -1,13 +1,30 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 const bodyParser = require('body-parser');
-
 const controllers = require('./controllers');
 const middlewares = require('./middlewares');
 
 const app = express();
 
 app.use(bodyParser.json());
+
+const corsOptions = {
+  origin(origin, callback) {
+    // if (origin === undefined || whiteList.indexOf(origin) !== -1) {
+    callback(null, true);
+    // } else {
+    // callback(new Error('BAD_CORS'));
+    // }
+  },
+  credentials: true,
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  maxAge: 86400,
+  methods: ['GET', 'POST', 'DELETE', 'PATCH', 'OPTIONS'],
+  optionsSuccessStatus: 204
+};
+
+app.use(cors(corsOptions));
 
 if (process.env.NODE_ENV !== 'test') {
   app.use(morgan('dev'));
