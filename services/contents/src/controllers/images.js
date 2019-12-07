5de4f9017a271fda9223d67c44c6b6a9bf5449b5
Antoine <antoine.ladinh@epitech.eu>
2019-12-07 16:48:24 +0100

    ajout des landscapes image

--- services/contents/src/controllers/images.js
+++ services/contents/src/controllers/images.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const sharp = require('sharp');
+const Path = require('path');
+const sizeOf = require('image-size');
 const { upload, fileFilter } = require('../configurations/multer');
 const addContent = require('../services/content/content-add');
 const { ApiError } = require('../configurations/error');
@@ -49,9 +52,21 @@ images.post(
         return next(new ApiError('CONTENTS_FORBIDDEN_WRITE'));
       }
 
-      const content = await addContent('image', req.params.postId, req.authId, req.urlPath);
+      const content = await addContent(
+        'image',
+        req.params.postId,
+        req.authId,
+        req.urlPath
+      );
       if (!content) return next(new ApiError('CONTENTS_INEXISTENT_CONTENT'));
-
+      const ext = Path.extname(req.file.originalname);
+      const nameSplit = req.file.filename.split('.');
+      const convertRatio = 1.77778;
+      const dimensions = sizeOf(req.file.path);
+      const convertedHeight = Math.round(dimensions.width / convertRatio);
+      await sharp(req.file.path)
+        .resize({ width: dimensions.width, height: convertedHeight })
+        .toFile(`${req.filePath}/${nameSplit[0]}_landscape${ext}`);
       return res.status(200).json({
         id: content.id,
         data: content.data
