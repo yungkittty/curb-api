c797159303f48062215892f00c0ce412364d56ce
Skaaara <44507943+Skaaara@users.noreply.github.com>
2019-12-08 16:17:07 +0100

    Image landscape (#117)
    * avatar preview
    
    * different quality for avatar previews
    
    * Chaging quality of avatars
    
    * ajout des landscapes image

--- services/contents/src/configurations/size/index.js
+++ services/contents/src/configurations/size/index.js
@@ -1,19 +1,93 @@
 const sizes = [
   {
-    name: 'extra_small',
-    size: parseInt(process.env.AVATAR_SIZE_EXTRA_SMALL, 10)
+    name: 'extra_small-compress-low',
+    size: 40, // process.env.AVATAR_SIZE_EXTRA_SMALL,
+    quality: 0
   },
   {
-    name: 'small',
-    size: parseInt(process.env.AVATAR_SIZE_SMALL, 10)
+    name: 'small-compress-low',
+    size: 60, // process.env.AVATAR_SIZE_SMALL,
+    quality: 0
   },
   {
-    name: 'medium',
-    size: parseInt(process.env.AVATAR_SIZE_MEDIUM, 10)
+    name: 'medium-compress-low',
+    size: 80, // process.env.AVATAR_SIZE_MEDIUM,
+    quality: 0
   },
   {
-    name: 'large',
-    size: parseInt(process.env.AVATAR_SIZE_LARGE, 10)
+    name: 'large-compress-low',
+    size: 100, // process.env.AVATAR_SIZE_LARGE,
+    quality: 0
+  },
+  {
+    name: 'extra_large-compress-low',
+    size: 200, // process.env.AVATAR_SIZE_EXTRA_LARGE,
+    quality: 0
+  },
+  {
+    name: 'extra_extra_large-compress-low',
+    size: 300, // process.env.AVATAR_SIZE_EXTRA_EXTRA_LARGE,
+    quality: 0
+  },
+  {
+    name: 'extra_small-compress-medium',
+    size: 40, // process.env.AVATAR_SIZE_EXTRA_SMALL,
+    quality: 1
+  },
+  {
+    name: 'small-compress-medium',
+    size: 60, // process.env.AVATAR_SIZE_SMALL,
+    quality: 1
+  },
+  {
+    name: 'medium-compress-medium',
+    size: 80, // process.env.AVATAR_SIZE_MEDIUM,
+    quality: 1
+  },
+  {
+    name: 'large-compress-medium',
+    size: 100, // process.env.AVATAR_SIZE_LARGE,
+    quality: 1
+  },
+  {
+    name: 'extra_large-compress-medium',
+    size: 200, // process.env.AVATAR_SIZE_EXTRA_LARGE,
+    quality: 1
+  },
+  {
+    name: 'extra_extra_large-compress-medium',
+    size: 300, // process.env.AVATAR_SIZE_EXTRA_EXTRA_LARGE,
+    quality: 1
+  },
+  {
+    name: 'extra_small-compress-high',
+    size: 40, // process.env.AVATAR_SIZE_EXTRA_SMALL,
+    quality: 2
+  },
+  {
+    name: 'small-compress-high',
+    size: 60, // process.env.AVATAR_SIZE_SMALL,
+    quality: 2
+  },
+  {
+    name: 'medium-compress-high',
+    size: 80, // process.env.AVATAR_SIZE_MEDIUM,
+    quality: 2
+  },
+  {
+    name: 'large-compress-high',
+    size: 100, // process.env.AVATAR_SIZE_LARGE,
+    quality: 2
+  },
+  {
+    name: 'extra_large-compress-high',
+    size: 200, // process.env.AVATAR_SIZE_EXTRA_LARGE,
+    quality: 2
+  },
+  {
+    name: 'extra_extra_large-compress-high',
+    size: 300, // process.env.AVATAR_SIZE_EXTRA_EXTRA_LARGE,
+    quality: 2
   }
 ];
 
--- services/contents/src/controllers/avatars.js
+++ services/contents/src/controllers/avatars.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const multer = require('multer');
-const sharp = require('sharp');
+const Jimp = require('jimp');
 const fs = require('fs-extra');
 const uuidv4 = require('uuid/v4');
 const axios = require('axios');
@@ -41,12 +41,16 @@ const sizes = require('../configurations/size');
 
 const avatar = express();
 
-async function writeFile(src, dest, size) {
-  await sharp(src)
-    .resize(size, size)
-    .toFile(dest);
+async function writeFile(src, dest, size, quality) {
+  Jimp.read(src)
+    .then(image =>
+      image
+        .resize(size, size)
+        .quality((quality + 1) * 10)
+        .write(dest)
+    )
+    .catch(error => console.log(error));
 }
-
 const userUpload = multer({
   fileFilter: (req, file, callback) => {
     if (!file.originalname.toLowerCase().match(/\.(jpg|jpeg|png|gif)$/)) {
@@ -61,7 +65,10 @@ const userUpload = multer({
       callback(null, path);
     },
     filename: (req, file, callback) => {
-      callback(null, `${uuidv4()}.${file.originalname.split('.').reverse()[0]}`);
+      callback(
+        null,
+        `${uuidv4()}.${file.originalname.split('.').reverse()[0]}`
+      );
     }
   })
 });
@@ -80,7 +87,10 @@ const groupUpload = multer({
       callback(null, path);
     },
     filename: (req, file, callback) => {
-      callback(null, `${uuidv4()}.${file.originalname.split('.').reverse()[0]}`);
+      callback(
+        null,
+        `${uuidv4()}.${file.originalname.split('.').reverse()[0]}`
+      );
     }
   })
 });
@@ -91,7 +101,9 @@ avatar.use('/groups/:groupId', async (req, res, next) => {
     const rep = await axios({
       method: 'get',
       headers: { Cookie: `token=${req.cookies.token}` },
-      url: `http://curb-groups:4000/permissions/${req.params.groupId}/${req.authId}`,
+      url: `http://curb-groups:4000/permissions/${req.params.groupId}/${
+        req.authId
+      }`,
       validateStatus: undefined
     });
     if (rep.status !== 200) {
@@ -100,10 +112,18 @@ avatar.use('/groups/:groupId', async (req, res, next) => {
     if (!rep.data.creator) {
       return next(new ApiError('CONTENTS_NOT_GROUP_CREATOR'));
     }
-    const result = await directoryExists(`./uploads/avatars/groups/${req.params.groupId}`);
+    const result = await directoryExists(
+      `./uploads/avatars/groups/${req.params.groupId}`
+    );
     if (result) {
-      const files = await fs.readdir(`./uploads/avatars/groups/${req.params.groupId}`);
-      files.forEach(file => fs.unlink(Path.join(`./uploads/avatars/groups/${req.params.groupId}`, file)));
+      const files = await fs.readdir(
+        `./uploads/avatars/groups/${req.params.groupId}`
+      );
+      files.forEach(file =>
+        fs.unlink(
+          Path.join(`./uploads/avatars/groups/${req.params.groupId}`, file)
+        )
+      );
     }
     return next();
   } catch (error) {
@@ -117,10 +137,14 @@ avatar.use('/users/:userId', async (req, res, next) => {
     if (req.params.userId !== req.authId) {
       throw new ApiError('CONTENTS_FORBIDEN_OPERATION');
     }
-    const result = await directoryExists(`./uploads/avatars/users/${req.authId}`);
+    const result = await directoryExists(
+      `./uploads/avatars/users/${req.authId}`
+    );
     if (result) {
       const files = await fs.readdir(`./uploads/avatars/users/${req.authId}`);
-      files.forEach(file => fs.unlink(Path.join(`./uploads/avatars/users/${req.authId}`, file)));
+      files.forEach(file =>
+        fs.unlink(Path.join(`./uploads/avatars/users/${req.authId}`, file))
+      );
     }
     return next();
   } catch (error) {
@@ -128,66 +152,96 @@ avatar.use('/users/:userId', async (req, res, next) => {
   }
 });
 
-avatar.post('/groups/:groupId', groupUpload.single('file'), async (req, res, next) => {
-  const ext = Path.extname(req.file.originalname);
-  const fragment = new Date().getTime();
+avatar.post(
+  '/groups/:groupId',
+  groupUpload.single('file'),
+  async (req, res, next) => {
+    const ext = Path.extname(req.file.originalname);
+    const fragment = new Date().getTime();
 
-  const basePath = `./${process.env.AVATAR_DIRECTORIES_GROUP_PATH}${req.params.groupId}/${fragment}-`;
+    const basePath = `./${process.env.AVATAR_DIRECTORIES_GROUP_PATH}${
+      req.params.groupId
+    }/${fragment}-`;
 
-  const urlPath = `/${process.env.SERVICE_NAME}/${process.env.AVATAR_DIRECTORIES_GROUP_PATH}${req.params.groupId}/${fragment}-medium${ext}`;
+    const urlPath = `/${process.env.SERVICE_NAME}/${
+      process.env.AVATAR_DIRECTORIES_GROUP_PATH
+    }${req.params.groupId}/${fragment}-medium-compress-high${ext}`;
 
-  try {
-    await Promise.all(
-      sizes.map(size => writeFile(req.file.path, `${basePath}${size.name}${ext}`, size.size))
-    );
-    const response = await axios({
-      method: 'post',
-      data: {
+    try {
+      await Promise.all(
+        sizes.map(size =>
+          writeFile(
+            req.file.path,
+            `${basePath}${size.name}${ext}`,
+            size.size,
+            size.quality
+          )
+        )
+      );
+      const response = await axios({
+        method: 'post',
+        data: {
+          avatarUrl: urlPath
+        },
+        url: `http://curb-groups:4000/avatars/${req.params.groupId}`,
+        validateStatus: undefined
+      });
+      if (response.status !== 200) {
+        throw new OtherServiceError(response);
+      }
+      return res.status(200).json({
         avatarUrl: urlPath
-      },
-      url: `http://curb-groups:4000/avatars/${req.params.groupId}`,
-      validateStatus: undefined
-    });
-    if (response.status !== 200) {
-      throw new OtherServiceError(response);
+      });
+    } catch (error) {
+      return next(error);
     }
-    return res.status(200).json({
-      avatarUrl: urlPath
-    });
-  } catch (error) {
-    return next(error);
   }
-});
+);
 
-avatar.post('/users/:userId', userUpload.single('file'), async (req, res, next) => {
-  const ext = Path.extname(req.file.originalname);
-  const fragment = new Date().getTime();
+avatar.post(
+  '/users/:userId',
+  userUpload.single('file'),
+  async (req, res, next) => {
+    const ext = Path.extname(req.file.originalname);
+    const fragment = new Date().getTime();
 
-  const basePath = `./${process.env.AVATAR_DIRECTORIES_USER_PATH}${req.params.userId}/${fragment}-`;
+    const basePath = `./${process.env.AVATAR_DIRECTORIES_USER_PATH}${
+      req.params.userId
+    }/${fragment}-`;
 
-  const urlPath = `/${process.env.SERVICE_NAME}/${process.env.AVATAR_DIRECTORIES_USER_PATH}${req.params.userId}/${fragment}-medium${ext}`;
+    const urlPath = `/${process.env.SERVICE_NAME}/${
+      process.env.AVATAR_DIRECTORIES_USER_PATH
+    }${req.params.userId}/${fragment}-medium-compress-high${ext}`;
 
-  try {
-    await Promise.all(
-      sizes.map(size => writeFile(req.file.path, `${basePath}${size.name}${ext}`, size.size))
-    );
-    const response = await axios({
-      method: 'post',
-      data: {
+    try {
+      await Promise.all(
+        sizes.map(size =>
+          writeFile(
+            req.file.path,
+            `${basePath}${size.name}${ext}`,
+            size.size,
+            size.quality
+          )
+        )
+      );
+      const response = await axios({
+        method: 'post',
+        data: {
+          avatarUrl: urlPath
+        },
+        url: `http://curb-users:4000/avatars/${req.params.userId}`,
+        validateStatus: undefined
+      });
+      if (response.status !== 200) {
+        throw new OtherServiceError(response);
+      }
+      return res.status(200).json({
         avatarUrl: urlPath
-      },
-      url: `http://curb-users:4000/avatars/${req.params.userId}`,
-      validateStatus: undefined
-    });
-    if (response.status !== 200) {
-      throw new OtherServiceError(response);
+      });
+    } catch (error) {
+      return next(error);
     }
-    return res.status(200).json({
-      avatarUrl: urlPath
-    });
-  } catch (error) {
-    return next(error);
   }
-});
+);
 
 module.exports = avatar;
