489f10b62fa9ecb8249b8c9f33363b545aed5244
QuentinCln <32776491+QuentinCln@users.noreply.github.com>
2019-12-13 13:43:56 +0100

    [GROUPS] returning back array of userIds in getGroups. (#127)
    * returning back array of userIds
    
    * delete logs

--- services/groups/src/services/list-from-id.js
+++ services/groups/src/services/list-from-id.js
@@ -6,24 +6,31 @@ async function listFromId(groupIds, userId = undefined) {
     _id: { $in: groupIds }
   });
   const response = groups.map(async (group) => {
-    const userInGroup = await isUserInGroup(group._id, userId);
-    if (group.status === 'private' && (!userInGroup || !userId)) {
-      const {
-        id, name, avatarUrl, theme, status, description, category
-      } = group.getPublicFields();
-      return {
-        id,
-        name,
-        avatarUrl,
-        theme,
-        status,
-        description,
-        category
-      };
-    }
+    // const userInGroup = await isUserInGroup(group._id, userId);
+    // if (group.status === 'private' && (!userInGroup || !userId)) {
+    //   const {
+    //     id,
+    //     name,
+    //     avatarUrl,
+    //     theme,
+    //     status,
+    //     description,
+    //     category
+    //   } = group.getPublicFields();
+    //   return {
+    //     id,
+    //     name,
+    //     avatarUrl,
+    //     theme,
+    //     status,
+    //     description,
+    //     category
+    //   };
+    // }
     // TODO when group are private/ghost/public
     // skip ==>
-    return group.getPublicFields();
+    const grp = group.getPublicFields();
+    return { ...grp, users: grp.users.map(user => user.userId) };
   });
   return await Promise.all(response);
 }
